e426934566eb64a80c0c3f2f7ccbfaf48be981d9
chilliconkearney <59411530+chilliconkearney@users.noreply.github.com>
2021-08-02 13:42:37 +0100

    Add files via upload

---
+++ openProgram/openProgramTimer.bat
@@ -0,0 +1,3 @@
+pip install psutil
+
+python openProgramTimer.py
---
+++ openProgram/openProgramTimer.py
@@ -0,0 +1,71 @@
+import psutil
+import time
+
+timeOpen = 0
+
+Open = False
+
+#'\n' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+
+logFileName = 'programTimeLog.txt'
+configFileName = 'config.txt'
+
+with open(logFileName, 'a') as openFile:
+    writeStr = '\n' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Program Started'
+    
+    openFile.write(writeStr)
+    openFile.close()
+
+with open(configFileName, 'r') as reader:
+    step = 0 # a counter for how many lines have been read
+    for line in reader:
+        if step == 0: # if the line being read is the first line
+            watchPrograms = line.split(',')
+        
+        if step == 1:
+            timeIncrement = int(line) * 60
+
+        step += 1
+
+print(watchPrograms)
+
+
+while True:
+    time.sleep(1)
+    
+    openFile = open(logFileName, 'a')
+
+    processList = []
+
+    for i in psutil.process_iter(): # adding the processes that have an accessible name to a list
+        try:
+            processList.append(i.name())
+
+        except psutil.AccessDenied:
+            pass
+    
+    if "chrome.exe" in processList: # checks if the program is running
+        timeOpen += 1
+
+        if Open == False:
+            writeStr = '\n' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Chrome Opened'
+            openFile.write(writeStr)
+            Open = True
+        
+        if timeOpen%timeIncrement == 0:
+            writeStr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Chrome open for ' + str(timeOpen)
+            openFile.write(writeStr)            
+
+    else: 
+        if Open == True:
+            writeStr = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Chrome open for ' + str(timeOpen)
+            openFile.write(writeStr)
+            writeStr = '\n' + time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())) + ' Chrome Closed'
+            openFile.write(writeStr)
+            
+            timeOpen = 0
+            Open = False
+
+    openFile.close()
+
+
